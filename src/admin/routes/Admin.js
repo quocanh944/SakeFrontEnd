d3aa850fa78f0108ea9100da8ca368ec03d3f903
unknown <quocanh944@gmail.com>
2023-08-27 22:34:13 +0700

    Fix layout sign up

--- src/admin/routes/Admin.js
+++ src/admin/routes/Admin.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "../components/AppLayout";
+import AuthenLayout from "../components/AuthenLayout";
 import Dashboard from "../pages/Dashboard";
 import Orders from "../pages/Orders";
 import Products from "../pages/Products";
@@ -15,15 +16,17 @@ const Admin = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<Dashboard />}></Route>
-        <Route path="/order" element={<Orders />}></Route>
-        <Route path="/product" element={<Products />}></Route>
-        <Route path="/customer" element={<Customer />}></Route>
-        <Route path="/create-new-campaign" element={<CampaignForm />}></Route>
-        <Route path="/brands" element={<Brands />}></Route>
-        <Route path="/films" element={<Films />}></Route>
-        <Route path="/add-product" element={<ProductForm />}></Route>
+      <Route path="/" element={<AuthenLayout />}>
+        <Route path="" element={<AppLayout />}>
+          <Route index element={<Dashboard />}></Route>
+          <Route path="/order" element={<Orders />}></Route>
+          <Route path="/product" element={<Products />}></Route>
+          <Route path="/customer" element={<Customer />}></Route>
+          <Route path="/create-new-campaign" element={<CampaignForm />}></Route>
+          <Route path="/brands" element={<Brands />}></Route>
+          <Route path="/films" element={<Films />}></Route>
+          <Route path="/add-product" element={<ProductForm />}></Route>
+        </Route>
         <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
