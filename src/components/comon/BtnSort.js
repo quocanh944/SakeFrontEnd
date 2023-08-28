0bf3c056168850ee543ad43858304e763391b378
SoMuch <minhnhieu50@gmail.com>
2023-08-29 00:35:05 +0700

    upgrade feature:filter & sort

--- src/components/comon/BtnSort.js
+++ src/components/comon/BtnSort.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-export default function btnSort() {
+export default function btnSort({handleSort}) {
     return (
         <div className='max-w-[160px] h-9 flex justify-evenly gap-2 items-center border-2 tracking-[-0.3px] p-3 mb-[9px]'>
             <div className='text-[13px] leading-[26px] opacity-50'>Sort By</div>
-            <select id="sort" className='bg-backgroundprimary text-[14px] font-bold leading-[26px] hover:cursor-pointer'>
+            <select id="sort" className='bg-backgroundprimary text-[14px] font-bold leading-[26px] hover:cursor-pointer' onChange={(e) => {
+                handleSort(e.target.value)
+            }}>
                 {/* css option??? */}
-                <option value="Popular">Popular</option>
-                <option value="Price">Price</option>
-                <option value="Color">Color</option>
+                <option value="ASC">ASC</option>
+                <option value="DESC">DESC</option>
+                {/* <option value="Color">Color</option> */}
             </select>
         </div>
     )
--- src/pages/ShopMain.js
+++ src/pages/ShopMain.js
@@ -7,14 +7,16 @@ export default function ShopMain() {
     //data sample
     const [products, setProducts] = useState([])
     const [page, setPage] = useState(0)
+
     const [numProduct, setNumProduct] = useState(0)
     const [brands, setBrands] = useState([])
     const [films, setFilms] = useState([])
 
+
     const [productFilter, setProductFilter] = useState([])
 
 
-    const [sort, setSort] = useState()
+    const [sort, setSort] = useState('ASC')
     const [brandFilter, setBrandFilter] = useState([])
     const [filmFilter, setFilmFilter] = useState([])
     const [filters, setFilters] = useState([])
@@ -28,7 +30,7 @@ export default function ShopMain() {
                 params: {
                     page: page,
                     size: 6,
-                    sort: 'ASC'
+                    sort: sort
                 }
             })
             .then(res => {
@@ -36,8 +38,9 @@ export default function ShopMain() {
                 setTotalPage(res.data.totalPages)
                 setNumProduct(res.data.totalElements)
             })
-    }, [page])
+    }, [page, sort])
     useEffect(() => {
+        // numproduct? page?
         if (brandFilter || filmFilter) {
             let filterBrand = async () => {
                 brandFilter.map(async id => {
@@ -49,7 +52,6 @@ export default function ShopMain() {
                         }
                     })
                     setProductFilter(prePro => mergeProducts(prePro, result.data.content, 'id'))
-                    setNumProduct(productFilter.length)
                 })
             }
             let filterFilm = async () => {
@@ -62,34 +64,26 @@ export default function ShopMain() {
                         }
                     })
                     setProductFilter(prePro => mergeProducts(prePro, result.data.content, 'id'))
-                    setNumProduct(productFilter.length)
                 })
             }
             filterBrand()
             filterFilm()
-            setProductFilter(prePr => {
-                return [...new Set(prePr)]
-            })
         }
         if ((brandFilter.length === 0) && (filmFilter.length === 0)) {
             setProductFilter(() => [])
         }
     }, [page, filmFilter, brandFilter])
 
-    console.log('Sumon product:', products);
-
     useEffect(() => {
         axios
             .get('http://localhost:8080/api/brands')
             .then(res => {
                 setBrands(res.data)
-                console.log(brands);
             })
         axios
             .get('http://localhost:8080/api/films')
             .then(res => {
                 setFilms(res.data)
-                console.log(films);
             })
     }, [])
 
@@ -97,23 +91,45 @@ export default function ShopMain() {
     // setProductFilter(prePr => {
     //     return [...new Set(prePr)]
     // })
+    
 
-    // console.log('filter brand: ', brandFilter);
-    // console.log('filter film: ', filmFilter);
+    console.log('filter brand: ', brandFilter);
+    console.log('filter film: ', filmFilter);
+    const handleRemoveProductByBrand = (id) => {
+        setProductFilter(pre => {
+            pre.filter(item => item?.id === id)
+        })
+    }
+    const handleRemoveProductByFilm = (id) => {
+        setProductFilter(pre => {
+            pre.filter(item => item?.id === id)
+        })
+    }
+    console.log(productFilter);
 
+    const sortProductFilter = (pre, sortValue) => {
+        switch (sortValue) {
+            case 'ASC':
+                return pre.slice().sort((a, b) => a.price - b.price)
+            case 'DESC':
+                return pre.slice().sort((a, b) => b.price - a.price)
+            default:
+                break;
+        }
+    }
+    useEffect(() => {
+        setProductFilter(pre => sortProductFilter(pre, sort))
+    }, [sort, productFilter])
 
     const mergeProducts = (productsA = [], productsB = [], id) => {
-        const mergedProducts = [...productsB];
-
-        for (const obj2 of productsA) {
+        const mergedProducts = [...productsA];
+        for (const obj2 of productsB) {
             const value2 = obj2[id];
             const exists = mergedProducts.some(obj1 => obj1[id] === value2);
-
             if (!exists) {
                 mergedProducts.push(obj2);
             }
         }
-
         return mergedProducts;
     }
 
@@ -123,7 +139,12 @@ export default function ShopMain() {
                 <div className='pt-[43px] ml-[145px]'>
                     <div className='mb-5'>
                         <div className='inline-block w-[75px] text-[22px] font-semibold leading-[30px] tracking-[-0.55px] mr-2'>Filters</div>
-                        <button type='reset' className='inline-block text-[#C4C4C4] text-[14px] leading-5 tracking-[-0.4px] underline'>Clear filters</button>
+                        <button type='reset' className='inline-block text-[#C4C4C4] text-[14px] leading-5 tracking-[-0.4px] underline' onClick={()=> {
+                            setBrandFilter([])
+                            setFilmFilter([])
+                            document.querySelectorAll('.brand').forEach(item => item.checked = false)
+                            document.querySelectorAll('.film').forEach(item => item.checked = false)
+                        }}>Clear filters</button>
                     </div>
                     <div className='text-[14px] font-bold leading-5 tracking-[-0.4px] mb-5'>
                         Brands
@@ -134,11 +155,13 @@ export default function ShopMain() {
                             {brands.map((brand, index) => {
                                 return (
                                     <li key={index} className='flex gap-3 max-w-[190px] text-[13px] leading-[17px] tracking-[-0.4px] items-center'>
-                                        <input type='checkbox' className='w-5 h-5 accent-primary mr-3 cursor-pointer' name={brand.id} onClick={(e) => {
+                                        <input type='checkbox' className='brand w-5 h-5 accent-primary mr-3 cursor-pointer' name={brand.id} onClick={(e) => {
                                             setPage(0)
                                             setBrandFilter((preBrands) => {
-                                                if (preBrands.find(item => item === e.target.name))
+                                                if (preBrands.find(item => item === e.target.name)) {
+                                                    handleRemoveProductByBrand(e.target.name)
                                                     return preBrands.filter(item => (item !== e.target.name))
+                                                }
                                                 return [...preBrands, e.target.name]
                                             })
                                         }} />
@@ -157,11 +180,13 @@ export default function ShopMain() {
                             {films.map((film, index) => {
                                 return (
                                     <li key={index} className='flex gap-3 max-w-[190px] text-[13px] leading-[17px] tracking-[-0.4px] items-center'>
-                                        <input type='checkbox' className='w-5 h-5 accent-primary mr-3 cursor-pointer' name={film.id} onClick={(e) => {
+                                        <input type='checkbox' className='film w-5 h-5 accent-primary mr-3 cursor-pointer' name={film.id} onClick={(e) => {
                                             setPage(0)
                                             setFilmFilter((preFilms) => {
-                                                if (preFilms.find(item => item === e.target.name))
+                                                if (preFilms.find(item => item === e.target.name)) {
+                                                    handleRemoveProductByFilm(e.target.name)
                                                     return preFilms.filter(item => (item !== e.target.name))
+                                                }
                                                 return [...preFilms, e.target.name]
                                             })
                                         }} />
@@ -175,7 +200,11 @@ export default function ShopMain() {
                 <div className='flex flex-col mt-[46px] mr-[142px]'>
                     <div className='flex flex-col items-end'>
                         {/* sort  option*/}
-                        <BtnSort></BtnSort>
+                        <BtnSort handleSort={(sortValue) => {
+                            setProducts([])
+                            setPage(0)
+                            setSort(sortValue)
+                        }}></BtnSort>
                         {/* number of products */}
                         <div className='h-7 text-[14px] leading-5 tracking-[-0.4px] mb-[18px]'>Total: {numProduct} Products</div>
                     </div>
@@ -184,7 +213,7 @@ export default function ShopMain() {
 
                             (brandFilter.length === 0) && (filmFilter.length === 0) ? products.map((product, index) => {
                                 return <ProductCard key={index} id={product.id} name={product.name} price={product.price} quantity={product.quantity} media={product.medias[0]} description={product.description} flim={product.flim} brand={product.brand} discounted='0' />
-                            }) : productFilter.map((product, index) => {
+                            }) : productFilter?.map((product, index) => {
                                 return <ProductCard key={index} id={product.id} name={product.name} price={product.price} quantity={product.quantity} media={product.medias[0]} description={product.description} flim={product.flim} brand={product.brand} discounted='0' />
                             })
                         }
